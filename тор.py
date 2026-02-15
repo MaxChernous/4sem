7d11a996eb1485e203ba6bfdb6caa4c2c849ad86
MaxChernous <94230811+MaxChernous@users.noreply.github.com>
2026-02-15 14:56:20 +0300

    Сделал область между внутренним и внешним торами

--- тор.py
+++ тор.py
@@ -7,21 +7,27 @@ import sys
 gmsh.initialize()
 gmsh.model.add("torus_mesh")
 
-# torus: (x, y, z, R, r)
-R = 0.5
-r = 0.2
+# Внешний и внутренний торы
+torus_1 = gmsh.model.occ.addTorus(0, 0, 0, 1, 0.2)
+torus_2 = gmsh.model.occ.addTorus(0, 0, 0, 1, 0.3)
+gmsh.model.occ.cut([(3, torus_2)], [(3, torus_1)])
 
-torus_tag = gmsh.model.occ.addTorus(0, 0, 0, R, r)
+# Синхронизируем геометрию с моделью
 gmsh.model.occ.synchronize()
 
 # Настройка плотности сетки
+# Задаем размер ячейки через "Field" (поле), чтобы сетка была равномерной
 gmsh.model.mesh.setSize(gmsh.model.getEntities(0), 0.05)
 
+# Генерируем 3D сетку
 gmsh.model.mesh.generate(3)
+
+# Сохраняем результат
 gmsh.write("tor.msh")
 
-# Запуск визуализации
+# Запуск визуализации, если не указан флаг -nopopup
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
 
 gmsh.finalize()
+
